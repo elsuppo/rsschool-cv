5fb5e130d4b6adc401904b89a53c9bacab493761
elsuppo <andrew.suponev@gmail.com>
2022-03-29 22:12:16 +0300

    feat: add sertificates in education

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ const navItems = document.querySelectorAll('.nav-item');
 function burgerMenu() {
   nav.classList.toggle('nav-open');
   burger.classList.toggle('is-active');
-  shadow.classList.toggle('shadow-enabled');
 };
 burger.addEventListener('click', burgerMenu);
 
 for (let navItem of navItems) {
   navItem.addEventListener('click', () => {
     nav.classList.remove('nav-open');
-    shadow.classList.remove('shadow-enabled');
     burger.classList.remove('is-active');
   });
 };
